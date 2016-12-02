11406986f5034978b12dc31655539eb760839bd5
dwanderson <anderson.dan.w@gmail.com>
2016-12-02 13:48:32 -0600

    runner script - checks out git-repo, runs src.main.main() with no arguments

---
+++ runner.py
@@ -0,0 +1,41 @@
+import os
+import sys
+import argparse
+import shutil
+import stat
+from git import Repo as gitrepo
+
+SCRATCH_DIR = os.path.join(os.getenv("HOME"), "scratch")
+
+
+def get_repo_dir(repo_name):
+    return os.path.join(SCRATCH_DIR, repo_name)
+
+
+def pull_gitrepo(repo_name):
+    ## if can't delete something because it's not read/write, just
+    ## make it read/write, then delete it
+    def del_rw(action, name, exc):
+        os.chmod(name, stat.S_IWRITE)
+        os.remove(name)
+
+    repo_url = "https://git.enova.com/danderson2/" + repo_name
+    repo_dir = get_repo_dir(repo_name)
+    if os.path.exists(repo_dir):
+        shutil.rmtree(repo_dir, onerror=del_rw)
+    gitrepo.clone_from(repo_url, repo_dir)
+
+
+def run_repo(repo_name):
+    pull_gitrepo(repo_name)
+    sys.path.insert(0, get_repo_dir(repo_name))
+    import src.main as repo_main
+    repo_main.main()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("repo-name", help="git repo name")
+    args = parser.parse_args()
+
+    run_repo(args.repo_name)
