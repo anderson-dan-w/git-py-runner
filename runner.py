d6cf04d9636a2a8be31d1af5602b322e626d9a3e
dwanderson <anderson.dan.w@gmail.com>
2017-01-18 21:44:28 -0600

    added github-url and github-user options, defaulting to my *public* account

--- runner.py
+++ runner.py
@@ -10,6 +10,7 @@ if sys.platform == "win32":
     os.environ["GIT_PYTHON_GIT_EXECUTABLE"] = git_path
 from git import Repo as gitrepo
 
+## *nix/Mac have $HOME, Windows has $HOMEPATH
 HOME = os.getenv("HOME") or os.getenv("HOMEPATH")
 SCRATCH_DIR = os.path.join(HOME, "scratch")
 
@@ -18,24 +19,25 @@ def get_repo_dir(repo_name):
     return os.path.join(SCRATCH_DIR, repo_name)
 
 
-def pull_gitrepo(repo_name):
+def pull_gitrepo(repo_name, github_url, github_user):
     ## if can't delete something because it's not read/write, just
     ## make it read/write, then delete it
     def del_rw(action, name, exc):
         os.chmod(name, stat.S_IWRITE)
         os.remove(name)
 
-    repo_url = "https://git.enova.com/danderson2/" + repo_name
+    ## create a local directory to hold the repo
     repo_dir = get_repo_dir(repo_name)
     if os.path.exists(repo_dir):
         shutil.rmtree(repo_dir, onerror=del_rw)
     if not os.path.exists(repo_dir):
         os.makedirs(repo_dir)
+
+    repo_url = "{}/{}/{}".format(github_url, github_user, repo_name)
     gitrepo.clone_from(repo_url, repo_dir)
 
 
 def run_repo(repo_name, repo_args):
-    pull_gitrepo(repo_name)
     sys.path.insert(0, get_repo_dir(repo_name))
     import src.main as repo_main
     repo_main.main(repo_args)
@@ -45,10 +47,15 @@ def main(args):
     parser = argparse.ArgumentParser()
     parser.add_argument("repo_name", metavar="repo-name",
         help="git repo name")
+    parser.add_argument("--github-url", default="https://github.com",
+        help="github URL base to access repo")
+    parser.add_argument("--github-user", default="anderson-dan-w",
+        help="github user whose repo to check out")
     ## allow users to pass args for repo.main(), even though we can't
     ## know what those args are yet - just bundle them up and pass along
     args, repo_args = parser.parse_known_args(args)
 
+    pull_gitrepo(args.repo_name, args.github_url, args.github_user)
     run_repo(args.repo_name, repo_args)
 
 if __name__ == "__main__":
